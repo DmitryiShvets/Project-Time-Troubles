70cd0c3a2e96cd98f33114a5bc07ab868b07c3ce
Alina <93431239+Alina0101@users.noreply.github.com>
2022-05-04 23:02:59 +0300

    finished with slash effect animation

--- Assets/Gameplay/Combat System/Scripts/AttackHand.cs
+++ Assets/Gameplay/Combat System/Scripts/AttackHand.cs
@@ -7,8 +7,10 @@ public class AttackHand : MonoBehaviour
 {
     [SerializeField] private Transform attackZonePrefab;
     [SerializeField] private Transform colliderAttackZoneObject;
+    private Transform slashTransform;
     [SerializeField]
     private Transform up;
+    private Vector3 vector_up = new Vector3 (0f, 1f, 0f);
     [SerializeField]
     private Transform down;
     [SerializeField]
@@ -23,6 +25,8 @@ public class AttackHand : MonoBehaviour
     private Transform downLeft;
     [SerializeField]
     private Transform downRight;
+
+    private string name_anim;
     //  bool start;
 
     Animator animator;
@@ -30,59 +34,92 @@ public class AttackHand : MonoBehaviour
     void Start()
     {
         colliderAttackZoneObject.position = up.position;
-      
+
     }
 
     private void Awake()
     {
         //cache the animator component
         animator = GetComponentInChildren<Animator>();
-     }
+    }
 
-   
+
     void Update()
     {
 
 
         if (Keyboard.current.fKey.wasPressedThisFrame)
         {
-          //  start = true;
+            //name_anim = "SlashEffectRight";
+            //  start = true;
             Instantiate(attackZonePrefab, colliderAttackZoneObject.position, colliderAttackZoneObject.rotation);
-         animator.Play("SlashEffect");
-                       
+            animator.Play(name_anim);
         }
 
         if (CheckDirection.instance.up)
         {
+            
             colliderAttackZoneObject.position = up.position;
+            name_anim = "SlashEffect";
+            var pos = up.position;
+            pos.y = up.position.y + 0.3f;
+            animator.transform.position = pos;
         }
         if (CheckDirection.instance.down)
         {
             colliderAttackZoneObject.position = down.position;
+            name_anim = "SlashEffectLeft";
+            var pos = down.position;
+            pos.y = down.position.y + 0.1f;
+            animator.transform.position = pos;
         }
         if (CheckDirection.instance.left)
         {
             colliderAttackZoneObject.position = left.position;
+            name_anim = "SlashEffectLeft";
+            var pos = left.position;
+            pos.y = left.position.y + 0.5f;
+            animator.transform.position = pos;
         }
         if (CheckDirection.instance.right)
         {
             colliderAttackZoneObject.position = right.position;
+            name_anim = "SlashEffect";
+            var pos = right.position;
+            pos.y = right.position.y + 0.5f;
+            animator.transform.position = pos;
         }
         if (CheckDirection.instance.upLeft)
         {
             colliderAttackZoneObject.position = upLeft.position;
+            name_anim = "SlashEffectLeft";
+            var pos = upLeft.position;
+            pos.y = upLeft.position.y + 0.3f;
+            animator.transform.position = pos;
         }
         if (CheckDirection.instance.upRight)
         {
             colliderAttackZoneObject.position = upRight.position;
+            name_anim = "SlashEffect";
+            var pos = upRight.position;
+            pos.y = upRight.position.y + 0.3f;
+            animator.transform.position = pos;
         }
         if (CheckDirection.instance.downLeft)
         {
             colliderAttackZoneObject.position = downLeft.position;
+            name_anim = "SlashEffectLeft";
+            var pos = downLeft.position;
+            pos.y = downLeft.position.y + 0.8f;
+            animator.transform.position = pos;
         }
         if (CheckDirection.instance.downRight)
         {
             colliderAttackZoneObject.position = downRight.position;
+            name_anim = "SlashEffect";
+            var pos = downRight.position;
+            pos.y = downRight.position.y + 0.8f;
+            animator.transform.position = pos;
         }
 
         attackZonePrefab.position = colliderAttackZoneObject.position;
