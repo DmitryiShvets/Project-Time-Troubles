462132a4921ca71f5d696b9296ef6f6952468d22
DmitryiShvets <45293708+DmitryiShvets@users.noreply.github.com>
2022-05-08 15:58:40 +0300

    prikol commit

--- Assets/Gameplay/Combat System/Scripts/DamageEnemy.cs
+++ Assets/Gameplay/Combat System/Scripts/DamageEnemy.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -26,6 +27,27 @@ public class DamageEnemy : MonoBehaviour
 
         }
     }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy"))
+        {
+
+            collision.gameObject.GetComponent<HealthSystemEnemy>().Damage(damage);
+            Destroy(gameObject);
+            
+        }
+     
+        if (collision.gameObject.CompareTag("Loot"))
+        {
+
+            collision.gameObject.GetComponent<BreakObject>().Damage(damage);
+            Destroy(gameObject);
+
+        }
+    }
+
+    
+    
 
     private void Update()
     {
